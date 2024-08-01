4bd09d3716a45fc1548880916550054a50e0bb74
NicoDiDomenico <nico.a.didomenico@gmail.com>
2024-08-01 02:04:33 -0300

    mas avance

--- DAW/Final/form.js
+++ DAW/Final/form.js
@@ -13,32 +13,26 @@ var emailError = document.getElementById('emailError')
 var messageError = document.getElementById('messageError')
 /* Me parece que al id lo uso para el JS y a la clase para el CSS */
 
-var validateAndSendForm = function (e) { /* esta forma permite manipular la varibale para agregarle otra funcion o pasarla como argumento, la otra forma es: function validateAndSendForme(e){...} */
-  e.preventDefault()
-
-  let valido = true
+/* funcion anónima */
+var validateAndSendForm = function (e) { /* esta forma permite manipular la variable para agregarle otra funcion o pasarla como argumento, la otra forma es: function validateAndSendForme(e){...} */
+  e.preventDefault() /*  Esta línea previene el comportamiento por defecto del formulario, que sería recargar la página al enviarse. */
 
   //Valido el nombre
-  if (nameInput.value.trim() === '') {
+  if (nameInput.value.trim() === '') { /* . trim() elimina los espacios en blanco al inicio y al final del valor. */
     nameError.textContent = 'El nombre es obligatorio'
-    valido = false
-  } else if (!/^[a-zA-Z0-9 ]+$/.test(nameInput.value)) {
-    nameError.textContent =
-      'El nombre solo puede contener letras, números y espacios'
-    valido = false
+  } else if (!(/^[a-zA-Z0-9 ]+$/).test(nameInput.value)) { /* El método .test() en JavaScript se utiliza para comprobar si una cadena de texto cumple con un patrón específico definido por una expresión regular */
+    nameError.textContent = 'El nombre solo puede contener letras, números y espacios'
   } else {
-    nameError.textContent = ''
+    nameError.textContent = '' /* para mi esto está al pedo */
   }
 
   //Valido el email
   if (emailInput.value.trim() === '') {
     emailError.textContent = 'El correo electrónico es obligatorio'
-    valido = false
   } else if (
     !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(emailInput.value)
   ) {
     emailError.textContent = 'El correo electrónico no es válido'
-    valido = false
   } else {
     emailError.textContent = ''
   }
@@ -46,17 +40,21 @@ var validateAndSendForm = function (e) { /* esta forma permite manipular la vari
   //Valido el mensaje
   if (messageInput.value.trim().length < 5) {
     messageError.textContent = 'El mensaje debe tener al menos 5 caracteres'
-    valido = false
   } else {
     messageError.textContent = ''
   }
 
   //Abro el email del sist. operativo con los datos del form
-  if (valido) {
-    var email = `mailto:agusalbo2024@gmail.com?subject=Boggle-Contacto&body=${messageInput.value}`
-
+  if (nameError.textContent === '' && emailError.textContent === '' && messageError.textContent === '') 
+    var email = `mailto:agusalbo2024@gmail.com,nico.a.didomenico@gmail.com?subject=Boggle-Contacto&body=${messageInput.value}` /* `estos backticks se hacen con Alt Gr + Tecla` */
+    /* 
+    mailto: es un esquema de URI (Uniform Resource Identifier) que se usa para crear un enlace que abre el cliente de correo electrónico predeterminado del usuario con un nuevo mensaje prellenado. La estructura básica es mailto:email@example.com.
+    Parámetros de la URL: Después de la dirección de correo, puedes agregar parámetros como subject (asunto) y body (cuerpo del mensaje). Estos se añaden después de un signo de interrogación (?) y se separan por el signo &.
+    */
     window.location.href = email
-  }
+    /* 
+    Al asignar esta URL a window.location.href, el navegador abrirá el cliente de correo electrónico del usuario con un nuevo mensaje dirigido a agusalbo2024@gmail.com, con el asunto "Boggle-Contacto" y el cuerpo "Hola, este es mi mensaje".
+    */
 }
 
 contactForm.addEventListener('submit', validateAndSendForm)
