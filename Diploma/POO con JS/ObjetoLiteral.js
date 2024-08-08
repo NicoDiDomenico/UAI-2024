736366612422849bbfe25e3b46657e4b470ea97f
NicoDiDomenico <nico.a.didomenico@gmail.com>
2024-08-07 22:13:48 -0300

    avance

---
+++ Diploma/POO con JS/ObjetoLiteral.js
@@ -0,0 +1,26 @@
+// Notar que para JS ambos son objetos
+console.log(typeof({}));
+console.log(typeof([]));
+
+// Creando objetos
+var user = {
+    name: 'Nicolás',
+    lasname: 'Di Domenico',
+    age: 27,
+    hobbies: ['Vicar', 'Codear', 'Ver fútbol', 'Jugar con el Gino', 'Dormir (?'],
+    address: {
+        street: 'Javier Milei 737',
+        city: 'Imperio Libertario'
+    },
+    friends: [
+        {
+            name: 'Bruno',
+            phoneNamber: 123456789
+        },
+        {
+            name: 'Bruno',
+            phoneNamber: 123456789
+        }
+    ]
+}
+
