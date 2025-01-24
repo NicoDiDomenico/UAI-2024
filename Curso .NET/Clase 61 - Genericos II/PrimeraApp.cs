e406ed12c66a3d3b10ca457e87ae02dc0d838e43
NicoDiDomenico <nico.a.didomenico@gmail.com>
2025-01-23 23:22:34 -0300

    wdqwd

---
+++ Curso .NET/Clase 61 - Genericos II/PrimeraApp.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Channels;
+using System.Timers;
+using System.Xml;
+
+namespace AprendiendoGenericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Almacenando objetos tipo String
+            AlmacenObjetos<String> archivos = new AlmacenObjetos<String>(3); // Cuando instanciamos una clase que es generica debemos especificar en la instanciacion el tipo que vamos a instanciar + <T>
+
+            archivos.agregar("Juan");
+
+            archivos.agregar("Elena");
+
+            archivos.agregar("Antonio");
+                        
+            String nombrePersona = archivos.getElement(2); 
+
+            Console.WriteLine(nombrePersona);
+
+            // Almacenando objetos tipo Empleado
+            AlmacenObjetos<Empleado> empleados = new AlmacenObjetos<Empleado>(2);
+
+            empleados.agregar(new Empleado(1500));
+
+            empleados.agregar(new Empleado(2000));
+
+            Console.WriteLine(empleados.getElement(1).getSalario());
+
+        }
+    }
+
+    // Al reemplazar Object por <T> (generico) le estamos diciendo a C# oye esta clase va a manejar cualquier tipo de objeto (String, Empleado, Date, File, etc)
+    class AlmacenObjetos<T>
+    {
+        private T[] datosElemento; // Dentro de este array podemos almacenar cualquier tipo de objeto
+
+        private int i = 0;
+
+        public AlmacenObjetos(int z) 
+        {
+            datosElemento = new T[z];
+        }
+
+        public void agregar( T obj) // Este metodo va a poder agregar cualquier tipo de objeto que se pase por parámetros
+        {
+            datosElemento[i] = obj;
+
+            i++;
+        }
+
+        public T getElement(int i) // este geter va a devolver cualquier tipo de objeto
+        { 
+            return datosElemento[i];
+        }
+    }
+
+    class Empleado
+    {
+        private double salario;
+
+        public Empleado(double salario)
+        {
+            this.salario = salario;
+        }
+
+        public double getSalario()
+        {
+            return this.salario;
+        }
+    }
+}
---
+++ Curso .NET/Clase 69 - Delegados predicados y lambdas III/PrimeraApp.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DelegadosPredicadosLambdas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Uso del delegado
+            OperacionesMatematicas operation = new OperacionesMatematicas(Cuadrado);
+
+            // Aplicando Lamda
+            OperacionesMatematicas operation2 = new OperacionesMatematicas(n => n * n);
+
+            Console.WriteLine(operation(4));
+
+            Console.WriteLine();
+            // Otro uso:
+            List<int> numeros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            List<int> numerosPares = numeros.FindAll(i => i % 2 == 0);
+
+            // numerosPares.ForEach(numero => Console.WriteLine(numero));
+
+            numerosPares.ForEach(numero => { // Siempre que haya + de una linea de codigo se tendra queponer el codigo entre { ... }
+                Console.WriteLine("El número par es: ");
+                Console.WriteLine(numero);
+            });
+
+            Console.WriteLine();
+            // Usando Objetos con lambda:
+            Personas P1 = new Personas();
+            P1.Nombre = "Juana";
+            P1.Edad = 18;
+
+            Personas P2 = new Personas();
+            P2.Nombre = "María";
+            P2.Edad = 28;
+
+            /*ComparaPersonas comparaEdad = new ComparaPersonas((persona1, persona2) => persona1 == persona2);*/
+            ComparaPersonas comparaEdad = (persona1, persona2) => persona1 == persona2; // Es igual a lo de arriba pero ahorrando codigo
+
+            Console.WriteLine(comparaEdad(P1.Nombre, P2.Nombre));
+    }
+
+        public delegate int OperacionesMatematicas(int numero);
+
+        public delegate bool ComparaPersonas(String n1, String n2);
+        public static int Cuadrado(int num)
+        {
+            return num * num;
+        }
+
+    }
+
+    class Personas
+    {
+        private string nombre;
+        private int edad;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Edad { get => edad; set => edad = value; }
+    }
+}
