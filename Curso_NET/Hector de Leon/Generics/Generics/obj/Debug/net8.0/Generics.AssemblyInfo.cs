eb9656fc467a2474dc7f286e72b41f430e773268
NicoDiDomenico <nico.a.didomenico@gmail.com>
2025-08-08 11:07:32 -0300

    asdas

--- Curso_NET/Hector de Leon/Generics/Generics/obj/Debug/net8.0/Generics.AssemblyInfo.cs
+++ Curso_NET/Hector de Leon/Generics/Generics/obj/Debug/net8.0/Generics.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Generics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+644a52be88f2be49a34f5348f565e02d9f5cb76e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f374ae8c530d3ed154f352bc49ea8b892039249")]
 [assembly: System.Reflection.AssemblyProductAttribute("Generics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Generics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
